e78c8cc8373ff9d4bb15709112cb73b5ca926be1
Petel__ <petelauck@gmail.com>
2021-09-26 12:34:09 +0800

    wait forever instead of socket fork

--- runscript.sh
+++ runscript.sh
@@ -8,4 +8,5 @@ tail -f $(find $LOG_PATH -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | tail
 # Give enough time for a connection before trying to expose on 0.0.0.0:4003
 sleep 10
 echo "Initialized"
-socat TCP-LISTEN:4000,fork TCP:127.0.0.1:4000
+# socat TCP-LISTEN:4000,fork TCP:127.0.0.1:4000
+tail -f /dev/null
